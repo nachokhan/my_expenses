d797b2db0a3fe2389e47a21c568e17a04c6989c1
Nachokhan <ignacio.rigoni@gmail.com>
2020-05-13 22:23:53 -0300

    Added Class WTransaction to show a transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './wtransaction.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,21 +14,58 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      amount: 43.90,
+      date: DateTime.now(),
+      id: "1",
+      title: "Pan Casero",
+    ),
+    Transaction(
+      amount: 45,
+      date: DateTime.now(),
+      id: "2",
+      title: "Lechuga Casero",
+    )
+  ];
 
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("MY EXPenses"),
+        title: Text("My Expenses"),
       ),
-      body: Center(
-        child: Text("Hola"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.green,
+              child: Text("CHART"),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: <Widget>[
+              ...getTransactions(),
+            ],
+          ),
+        ],
       ),
     );
+  }
+
+  getTransactions() {
+    var list = transactions.map((tx) {
+      return WTransaction(
+        amount: tx.amount.toString(),
+        date: "Hoy",
+        title: tx.title,
+      );
+    }).toList();
+    return list;
   }
 }
---
+++ lib/wtransaction.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class WTransaction extends StatelessWidget {
+  final String title;
+  final String amount;
+  final String date;
+
+  WTransaction({
+    @required this.amount,
+    @required this.date,
+    @required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          WAmount("\$ $amount"),
+          WTitleDate(title, date),
+        ],
+      ),
+    );
+  }
+}
+
+class WTitleDate extends StatelessWidget {
+  final String title, date;
+
+  WTitleDate(this.title, this.date);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          Text(
+            date,
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WAmount extends StatelessWidget {
+  final String amount;
+
+  WAmount(this.amount);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.purple,
+          width: 3,
+          style: BorderStyle.solid,
+        ),
+      ),
+      child: Text(
+        amount,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+          color: Colors.purple,
+        ),
+        // textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
