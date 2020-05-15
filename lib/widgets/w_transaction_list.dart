209c5e8bbbf731730812171f7cd034cad16a57c9
Nachokhan <ignacio.rigoni@gmail.com>
2020-05-14 22:22:22 -0300

    Cahnged Column for ListView to show the items

--- lib/widgets/w_transaction_list.dart
+++ lib/widgets/w_transaction_list.dart
@@ -10,14 +10,46 @@ class WTransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ...getTransactions(),
-      ],
+    return Container(
+      height: 350,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (BuildContext ctx, int index) {
+          return Container(
+            child: getTransactionAt(index),
+          );
+        },
+
+      /*ALTERNATIVE to ListView.builder()
+
+        child: ListView(        
+        children: <Widget>[
+           ...getTransactionsList(),
+        ],
+        */
+
+      ),
     );
   }
 
-  getTransactions() {
+  /// This methods throws only one transaction based on
+  /// the passed index. It will be automatically called by
+  /// flutter when using ListView.builder() cause builder()
+  /// knows exactly which indexes has to build/paint/render
+  Widget getTransactionAt(int index) {
+    return Container(
+      child: WTransaction(
+        amount: transactions[index].amount,
+        date: transactions[index].date,
+        title: transactions[index].title,
+      ),
+    );
+  }
+
+  /// I Created this function to use inside a ListView
+  /// or a Column (although Column not recommended)
+  /// because it throws a complete list of WTransactions.
+  getTransactionsList() {
     var list = transactions.map((tx) {
       return WTransaction(
         amount: tx.amount,
