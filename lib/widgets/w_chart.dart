44306d57d63246f43c2f7a8726dfa2fc149b5045
Nachokhan <ignacio.rigoni@gmail.com>
2020-05-18 00:59:27 -0300

    Bug solved in bar creation

--- lib/widgets/w_chart.dart
+++ lib/widgets/w_chart.dart
@@ -34,70 +34,18 @@ class WChart extends StatelessWidget {
       totalAmount += value;
     });
 
-    for (int dayCounter = 0; dayCounter < 7; dayCounter++) {
-      var currentDate = DateTime.now().subtract(Duration(days: dayCounter));
+    amounts.forEach((key, value) {
+      var currentDate = DateTime.now().subtract(Duration(days: key - 1));
 
       final dia = DateFormat.E().format(currentDate);
 
-      // The idea behind 7-dayCounter is to use it in "reverse"
-      final percentage = totalAmount != 0
-          ? (amounts[(7 - dayCounter)] / totalAmount * 100).round()
-          : 0;
+      final percentage =
+          totalAmount != 0 ? (value / totalAmount * 100).round() : 0;
 
-      bars.add(WBar(dia, percentage, amounts[7 - dayCounter]));
-      bars.add(SizedBox(
-        width: 10,
-        height: 110,
-      ));
-    }
+      bars.add(WBar(dia, percentage, value));
+      bars.add(SizedBox(width: 10, height: 110));
+    });
+
     return bars;
-  }
-}
-
-class WBar extends StatelessWidget {
-  final String day;
-  final int percentage;
-  final double amount;
-  final tHeight = 110;
-  final width = 28.0;
-
-  WBar(this.day, this.percentage, this.amount);
-
-  @override
-  Widget build(BuildContext context) {
-    final hBox2 = (tHeight * (percentage) / 100);
-    final hBox1 = tHeight - hBox2;
-    return Container(
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            width: width,
-            height: hBox1,
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1,
-                  )),
-            ),
-          ),
-          SizedBox(
-            width: width,
-            height: hBox2,
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                  color: Colors.green,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1,
-                  )),
-            ),
-          ),
-          Text('\$' + amount.toString()),
-          Text(day),
-        ],
-      ),
-    );
   }
 }
